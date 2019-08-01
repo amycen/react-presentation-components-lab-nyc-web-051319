37fcd5a782f8e315751b688b48e29389865ba60b
Amy Cen <amy.cen@blackrock.com>
2019-08-01 17:13:36 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,5 +1,11 @@
 // Code SimpleComponent Here
 import React, Component from 'react'
 class SimpleComponent extends Component {
+  state = {
+    mood: "happy"
+  }
   
+  render (){
+    return <div>{this.state.mood}</div>
+  }
 }
